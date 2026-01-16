dcfa993b5cdae14839fbf701025799b67fa77bba
Aurele Jambert <95047153+Aurele879@users.noreply.github.com>
2026-01-16 23:14:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import customtkinter
 
 
-
-class Window:
-    def __init__(self):
+class Window: # Class defining a window and its functions
+    def __init__(self): # Class constructor
         self.root = customtkinter.CTk()
         self.root.geometry("1000x600")
         self.root.title("My App")
-        self.root.iconbitmap("assets/icon.ico")
         self.root.configure(fg_color="#FFFFFF")
         self.num = 0
         self.setupWidgets()
         
-        
-        
-    def setupWidgets(self):
+    def setupWidgets(self): # Fuction to create all the widgets of the whole window
         self.explButton = customtkinter.CTkButton(self.root,
                                                 command=self.nextPage,
                                                 fg_color="#348D5C",
@@ -26,22 +22,22 @@ class Window:
                                                 corner_radius=20,
                                                 font=("Arial", 25, "bold"))   
                 
-    def clearUi(self):
+    def clearUi(self): # Fuction to hide all the widgets from the screen
         self.explButton.place_forget()
 
-    def thirdPage(self):
+    def thirdPage(self): # Fuction to place all the widgets from the page
         self.clearUi()
         self.explButton.place(x=35, y=200)
 
-    def mainPage(self):
+    def mainPage(self): # Fuction to place all the widgets from the page
         self.clearUi()
         self.explButton.place(x=525, y=470)
 
-    def secondPage(self):
+    def secondPage(self): # Fuction to place all the widgets from the page
         self.clearUi()
         self.explButton.place(x=500, y=500)
 
-    def nextPage(self):
+    def nextPage(self): # Fuction to switch pages with an incremented variable
         if self.num < 2:
             self.num +=1
         else:
@@ -54,7 +50,7 @@ class Window:
         elif self.num == 2:
             self.thirdPage()
 
-    def display(self):
+    def display(self): # Fuction to display the window
         self.mainPage()
         self.root.mainloop()
         
